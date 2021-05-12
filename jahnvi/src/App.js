8b528ac15a782a9b6f58aafd24354cb2e06f1c9e
Jahnvi <jahnvi18patel@gmail.com>
2021-05-12 14:21:49 +0100

    navbar position

--- jahnvi/src/App.js
+++ jahnvi/src/App.js
@@ -1,5 +1,22 @@
+import React from 'react'
+import MyNavbar from './Navbar'
+
+
 function App() {
-  return <h1>Hello World</h1>
+
+
+  return (
+    <>
+      <MyNavbar />
+      <h1>Hello World</h1>
+    </>
+  )
+
+
+
+
+
+
 }
 
 export default App
---
+++ jahnvi/src/Navbar.js
@@ -0,0 +1,24 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import React from 'react'
+import logo from './assets/text-1620824259728.png'
+
+
+const MyNavbar = () => {
+
+  return (
+    <Navbar expand="lg" variant="light" className="justify-content-between">
+      <Navbar.Brand href="/"><img alt='logo' className="logo" src={logo} /></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse className="justify-content-end">
+        <Nav className="mr-sm-2">
+          <Nav.Link href="/projects">PROJECTS</Nav.Link>
+          <Nav.Link href="/info">INFO</Nav.Link>
+          <Nav.Link href="/journal">JOURNAL</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default MyNavbar
