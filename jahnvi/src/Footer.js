f0301e6f5a5900f403dd567c07d0e8e6dbd1949a
Jahnvi <jahnvi18patel@gmail.com>
2021-05-13 12:32:02 +0100

    copy without package

--- jahnvi/src/Footer.js
+++ jahnvi/src/Footer.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import LinkedIn from './assets/iconfinder_SocialMedia_LinkedIn-Outline_2959747.png'
+import React, { useState } from 'react'
+import LinkedIn from './assets/pngaaa.com-158429.png'
+import Mail from './assets/PikPng.com_email-png_581646.png'
+import GitHub from './assets/pngjoy.com_telegram-icon-github-icon-png-white-png-download_1164606.png'
 
 const Footer = () => {
 
+  const [copySuccess, setCopySuccess] = useState('')
+
+  const copyToClipBoard = async copyMe => {
+    try {
+      await navigator.clipboard.writeText(copyMe)
+      setCopySuccess('Copied!')
+    } catch (err) {
+      setCopySuccess('Failed to copy!')
+    }
+  }
 
   return (
     <div className="icons">
+      <button onClick={() => copyToClipBoard('jahnvi18patel@email.com')}>
+        <img src={Mail} alt="mail" className="icon" />
+      </button>
+      <div>
+        {copySuccess}
+      </div>
       <img src={LinkedIn} alt="linkedIn" className="icon" />
+      <img src={GitHub} alt="gitHub" className="icon" />
     </div>
   )
 }
