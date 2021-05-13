67d4e202576116b87626cd75b971677303793620
Jahnvi <jahnvi18patel@gmail.com>
2021-05-13 11:33:53 +0100

    responsive homepage

--- jahnvi/src/components/Homepage.js
+++ jahnvi/src/components/Homepage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import MyNavbar from '../Navbar'
 import Footer from '../Footer'
-import Background from '../assets/val-toch-BHeQQmZM8jE-unsplash.jpg'
 
 const Homepage = () => {
 
@@ -9,13 +8,8 @@ const Homepage = () => {
   return (
     <>
       <MyNavbar />
-      <div className="main-container">
-        <div className="homepage-container">
-          <img src={Background} alt="background" className="background" />
-        </div >
-        <h3 className="title">Junior <br /> Full Stack <br /> Web Developer</h3>
-        <Footer />
-      </div>
+      <h3 className="title">Junior <br /> Full Stack <br /> Web Developer</h3>
+      <Footer />
     </>
   )
 }
