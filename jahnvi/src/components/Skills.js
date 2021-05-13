2c39df59050985aa47cd8c280df392dbcc9bfbf9
Jahnvi <jahnvi18patel@gmail.com>
2021-05-13 14:00:44 +0100

    new page

---
+++ jahnvi/src/components/Skills.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../Navbar'
+import Footer from '../Footer'
+
+const Skills = () => {
+
+
+  return (
+    <>
+      <Navbar />
+      <div className="blank-background-overlay">
+        <h3>hello</h3>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default Skills
