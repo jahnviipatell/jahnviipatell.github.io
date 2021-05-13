9d566ec2e42cf081823757c1236c29bb2225309d
Jahnvi <jahnvi18patel@gmail.com>
2021-05-13 18:00:01 +0100

    responsive skills page

--- jahnvi/src/components/Skills.js
+++ jahnvi/src/components/Skills.js
@@ -2,11 +2,17 @@ import React from 'react'
 import Navbar from '../Navbar'
 import Footer from '../Footer'
 import Nodejs from '../assets/nodejs-icon-logo-black-and-white.png'
-import JavaScript from '../assets/pngwing.com.png'
+// import JavaScript from '../assets/JavaScript.png'
 import Reactjs from '../assets/PikPng.com_google-play-store-logo_2690057.png'
 import Python from '../assets/Python.png'
 import Django from '../assets/Django.png'
 import HTML from '../assets/HTML.png'
+import CSS from '../assets/CSS.png'
+// import MongoDB from '../assets/MongoDb.png'
+// import PostreSQL from '../assets/PostreSQL.png'
+import Git from '../assets/Git.png'
+// import Heroku from '../assets/Heroku.png'
+// import Bootstrap from '../assets/Bootstrap.png'
 
 const Skills = () => {
 
@@ -20,6 +26,7 @@ const Skills = () => {
             <h3>Hi, I&apos;m Jahnvi Patel</h3>
             <p>
               <br />
+              <br />
               I am a plainspoken junior developer regarded for my compassion, logic, and creativity. I have a background in mechanical engineering with a proven ability to serve all projects with a methodical approach and focus on inclusiveness. <br />
               <br />
               Changing careers to allow myself to see my solutions implemented and follow projects through to the end of their life-cycle. Motivated to work as part of a diverse team or as an individual contributor with a strong desire to continuously improve. </p>
@@ -29,31 +36,51 @@ const Skills = () => {
             <ul className="skills">
               <li className="skill">
                 <img className="skill-icon" src={Nodejs} alt="nodejs" />
-                <h6>Node.js</h6>
+                <p>Node.js</p>
               </li>
               <li className="skill">
-                <img className="skill-icon" src={JavaScript} alt="javaScript" />
-                <h6>JavaScript</h6>
+                {/* <img className="skill-icon" src={JavaScript} alt="javaScript" /> */}
+                <p>JavaScript</p>
               </li>
               <li className="skill">
                 <img className="skill-icon" src={Reactjs} alt="reactjs" />
-                <h6>React.js</h6>
-              </li>
-              <li className="skill">
-                <img className="skill-icon" src={Python} alt="python" />
-                <h6>Python</h6>
-              </li>
-              <li className="skill">
-                <img className="skill-icon" src={Django} alt="django" />
-                <h6>Django</h6>
+                <p>React.js</p>
               </li>
               <li className="skill">
                 <img className="skill-icon" src={HTML} alt="HTML" />
-                <h6>HTML</h6>
+                <p>HTML</p>
               </li>
               <li className="skill">
-                {/* <img className="skill-icon" src={} alt="HTML" /> */}
-                <h6>HTML</h6>
+                <img className="skill-icon" src={Python} alt="python" />
+                <p>Python</p>
+              </li>
+              <li className="skill">
+                <img className="skill-icon django" src={Django} alt="django" />
+                <p>Django</p>
+              </li>
+              <li className="skill">
+                <img className="skill-icon css" src={CSS} alt="css" />
+                <p>CSS</p>
+              </li>
+              <li className="skill">
+                {/* <img className="skill-icon" src={MongoDB} alt="MongoDB" /> */}
+                <p>MongoDB</p>
+              </li>
+              <li className="skill">
+                {/* <img className="skill-icon" src={PostgreSQL} alt="PostgreSQL" /> */}
+                <p>PostreSQL</p>
+              </li>
+              <li className="skill">
+                <img className="skill-icon" src={Git} alt="Git" />
+                <p>Git</p>
+              </li>
+              <li className="skill">
+                {/* <img className="skill-icon" src={Heroku} alt="Heroku" /> */}
+                <p>Heroku</p>
+              </li>
+              <li className="skill">
+                {/* <img className="skill-icon" src={Bootstrap} alt="Bootstrap" /> */}
+                <p>React Bootstrap</p>
               </li>
             </ul>
           </div>
