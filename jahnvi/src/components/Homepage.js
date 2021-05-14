443a923e2d2bcbf252b9c0a221ff5bb405407611
Jahnvi <jahnvi18patel@gmail.com>
2021-05-14 13:54:21 +0100

    new background

--- jahnvi/src/components/Homepage.js
+++ jahnvi/src/components/Homepage.js
@@ -12,10 +12,12 @@ const Homepage = () => {
         <div id="HOME">
           <h3 className="title">Junior <br /> Full Stack <br /> Web Developer</h3>
         </div>
-        <div className="arrow"></div>
-        <div className="new-page">
-          <Skills />
+        <div className="arrow-container">
+          <div className="arrow"></div>
         </div>
+      </div>
+      <div className="new-page">
+        <Skills />
       </div>
     </>
   )
