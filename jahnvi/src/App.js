72b73f8e92932fc613ccae30ba1210cb3a606495
Jahnvi <jahnvi18patel@gmail.com>
2021-05-15 01:07:00 +0100

    what

--- jahnvi/src/App.js
+++ jahnvi/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Homepage from './components/Homepage'
-// import Skills from './components/Skills'
-// import Projects from './components/Projects'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
@@ -12,25 +9,11 @@ function App() {
 
 
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Navbar />
-          <Homepage />
-          <Footer />
-        </Route>
-      </Switch>
-      {/* <Switch>
-        <Route path="/skills">
-          <Skills />
-        </Route>
-      </Switch>
-      <Switch>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-      </Switch> */}
-    </BrowserRouter>
+    <>
+      < Navbar />
+      <Homepage />
+      <Footer />
+    </>
   )
 
 
