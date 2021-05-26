d0d5a5e7a661a5c53d43e1952df4375dd44fd93e
Jahnvi <jahnvi18patel@gmail.com>
2021-05-26 17:50:23 +0100

    before using package

--- jahnvi/src/components/Me.js
+++ jahnvi/src/components/Me.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import SloveniaOne from '../assets/slovenia-one.jpg'
+import PhiphiBoats from '../assets/phiphi-boats.jpg'
 
 const Me = () => {
 
@@ -7,13 +9,18 @@ const Me = () => {
 
   return (
     <div id="me" className="new-page-three">
-      <div>
-        <ul className="me-ul">
-          <li>
-            <h3>hello</h3>
-          </li>
-        </ul>
-      </div>
+      {/* <div> */}
+      <h3 className="projects-header">Me.</h3>
+      <h6 className="projects-header">my life in pictures</h6>
+      <ul className="me-ul">
+        <li>
+          <img src={SloveniaOne} alt="slovenia-car" className="me-img" />
+        </li>
+        <li>
+          <img src={PhiphiBoats} alt="slovenia-car" className="me-img" />
+        </li>
+      </ul>
+      {/* </div> */}
     </div>
   )
 }
---
+++ jahnvi/src/components/Photos.js
@@ -0,0 +1,13 @@
+export const photos = [
+  {
+    src: '../assets/phiphi-boats.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '../assets/slovenia-one.jpg',
+    width: 1,
+    height: 1,
+  }
+
+]
