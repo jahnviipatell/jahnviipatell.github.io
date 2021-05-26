ccbad96d01e86b698cdba0744aed0755ecb6a739
Jahnvi <jahnvi18patel@gmail.com>
2021-05-26 18:27:55 +0100

    links working

--- jahnvi/src/components/Me.js
+++ jahnvi/src/components/Me.js
@@ -1,26 +1,47 @@
-import React from 'react'
-import SloveniaOne from '../assets/slovenia-one.jpg'
-import PhiphiBoats from '../assets/phiphi-boats.jpg'
+import React, { useState, useCallback } from 'react'
+import Gallery from 'react-photo-gallery'
+import Carousel, { Modal, ModalGateway } from 'react-images'
+import { photos } from './Photos'
+
 
 const Me = () => {
 
+  const [currentImage, setCurrentImage] = useState(0)
+  const [viewerIsOpen, setViewerIsOpen] = useState(false)
 
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index)
+    setViewerIsOpen(true)
+    console.log(photo)
+  }, [])
+
+  const closeLightbox = () => {
+    setCurrentImage(0)
+    setViewerIsOpen(false)
+  }
 
 
   return (
     <div id="me" className="new-page-three">
-      {/* <div> */}
       <h3 className="projects-header">Me.</h3>
       <h6 className="projects-header">my life in pictures</h6>
-      <ul className="me-ul">
-        <li>
-          <img src={SloveniaOne} alt="slovenia-car" className="me-img" />
-        </li>
-        <li>
-          <img src={PhiphiBoats} alt="slovenia-car" className="me-img" />
-        </li>
-      </ul>
-      {/* </div> */}
+      <div>
+        <Gallery photos={photos} onClick={openLightbox} />
+        <ModalGateway>
+          {viewerIsOpen ? (
+            <Modal onClose={closeLightbox}>
+              <Carousel
+                currentIndex={currentImage}
+                views={photos.map(x => ({
+                  ...x,
+                  srcset: x.srcSet,
+                  caption: x.title,
+                }))}
+              />
+            </Modal>
+          ) : null}
+        </ModalGateway>
+      </div>
     </div>
   )
 }
--- jahnvi/src/components/Photos.js
+++ jahnvi/src/components/Photos.js
@@ -1,6 +1,6 @@
 export const photos = [
   {
-    src: '../assets/phiphi-boats.jpg',
+    src: 'https://i.imgur.com/OBPvHBy.jpg',
     width: 4,
     height: 3,
   },
@@ -8,6 +8,11 @@ export const photos = [
     src: '../assets/slovenia-one.jpg',
     width: 1,
     height: 1,
+  },
+  {
+    src: 'https://source.unsplash.com/qDkso9nvCg0/600x799',
+    width: 3,
+    height: 4,
   }
 
 ]
